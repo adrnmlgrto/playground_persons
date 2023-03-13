785225d280a0000b77262bb7529b1b7ba8d589b9
Adrian Melegrito <adrn.mlgrto@gmail.com>
2023-03-13 19:18:30 +0800

    added views

--- person/views.py
+++ person/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .api import (
+    create_person,
+    get_person,
+    get_all_person,
+    update_person,
+    delete_person)
 
-# Create your views here.
+
+def homepage(request):
+    # Use API that gets all persons in the DB using endpoint "/api/persons/all"
+    persons = get_all_person()
+
+    # Render the "view" template with the persons variable
+    return render(request, 'view.html', {'persons': persons})
+
+
+def new_person(request):
+    # Use API that creates a person in the DB using endpoint "/api/persons/new"
+    if request.method == 'POST':
+        create_person(request)
+        return redirect('view')
+    else:
+        return render(request, 'create.html')
+
+
+def person_update(request, person_id):
+    # Use API that creates a person in the DB using endpoint "/api/persons/update"
+    update_person(request, person_id)
+    pass
+
+
+def person_delete(request):
+    pass
